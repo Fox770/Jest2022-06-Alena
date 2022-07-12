0d1a7394b799898d863fcb71c3262e96a4fda581
Yauheniya Sobal <vishenka101183@gmail.com>
2022-07-12 10:41:53 +0300

    Add tests attempt2

--- specs/homework3.spec.js
+++ specs/homework3.spec.js
@@ -1,16 +1,48 @@
 import { fullTrim, nameIsValid } from '../src/homework3';
 
-describe('Здесь могла бы быть ваша реклама', () => {
+describe('Тесирование функций из Homework3', () => {
+
   /**
    * Допишите несколько unit тестов для функции (вспоминаем тест-дизайн),
    * которая удаляет пробелы и табуляторы в начале и конце строки.
    */
 
-  test('Проверяем тримминг для пробела в начале слова', () => {
-    expect(fullTrim(' Это домашка')).toEqual('Это домашка');
-  
+  //тестируем, что функция успешно импортировалась 
+  test('Проверяем fullTrim определяется', () => {
+    expect(fullTrim).toBeDefined();
+  });
 
-  g
+  test('Проверяем fullTrim функция', () => {
+    expect(typeof fullTrim).toBe('function');
+  });
+  //тесируем удаление пробелов
+  test('Проверяем fullTrim для пробела в начале слова', () => {
+    expect(fullTrim(' Это домашка')).toEqual('Это домашка');
+  });
+  test('Проверяем fullTrim для пробела в конце слова', () => {
+    expect(fullTrim('Это домашка ')).toEqual('Это домашка');
+  });
+  test('Проверяем fullTrim для пробела в середине слова', () => {
+    expect(fullTrim('Это домашка')).toEqual('Это домашка');
+  });
+  test('Проверяем fullTrim для одних пробелов', () => {
+    expect(fullTrim(' ')).toEqual('');
+  });
+
+  //тесируем удаление табуляции
+  test('Проверяем fullTrim для табуляции в начале слова', () => {
+    expect(fullTrim(' Это домашка')).toEqual('Это домашка');
+  });
+  test('Проверяем fullTrim для табуляции в конце слова', () => {
+    expect(fullTrim('Это домашка  ')).toEqual('Это домашка');
+  });
+  test('Проверяем fullTrim для табуляции в середине слова', () => {
+    expect(fullTrim('Это  домашка')).toEqual('Это  домашка');
+  });
+  test('Проверяем fullTrim для одних табов', () => {
+    expect(fullTrim(' ')).toEqual('');
+  });
+
   /**
    * Напишите параметризированный unit (describe.each`table`) тест для функции,
    * которая проверяет валидность кличек котиков.
@@ -23,7 +55,35 @@ describe('Здесь могла бы быть ваша реклама', () => {
    Параметризированный тест - https://jestjs.io/docs/en/api#testeachtablename-fn-timeout
    */
 
+   test('Проверяем nameIsValid определяется', () => {
+    expect(nameIsValid).toBeDefined();
+  });
+
+  test('Проверяем nameIsValid функция', () => {
+    expect(typeof nameIsValid).toBe('function');
+  });
+
   test('Передать валидную кличку', () => {
     expect(nameIsValid('Имя')).toEqual(true);
   });
+
+
+
+test('Передать невалидную кличку', () => {
+  expect(nameIsValid('h')).toEqual(false);
+});
+
+
+test.each`
+  name    | expected
+  ${'cat'} | ${true} {}
+  ${'ca'} | ${true}
+  ${'kitty1'} | ${true}
+  ${'kit@#$ty1'} | ${true}
+  ${'k'} | ${false}
+  ${'black cat'} | ${false}
+  ${'  '} | ${false}
+`('nickname $name is $expected', ({name, expected}) => {
+  expect(nameIsValid(name)).toBe(expected);
+});
 });
